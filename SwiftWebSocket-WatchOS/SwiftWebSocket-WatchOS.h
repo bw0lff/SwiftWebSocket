34af52931be19ddeb321798ecf6b9defe9e7de23
Jorge Miguel <jorgefmiguel@gmail.com>
2015-11-20 23:31:42 +0000

    watch support

---
+++ SwiftWebSocket-WatchOS/SwiftWebSocket-WatchOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWebSocket-WatchOS.h
+//  SwiftWebSocket-WatchOS
+//
+//  Created by Jorge Miguel on 20/11/15.
+//  Copyright © 2015 ONcast, LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SwiftWebSocket-WatchOS.
+FOUNDATION_EXPORT double SwiftWebSocket_WatchOSVersionNumber;
+
+//! Project version string for SwiftWebSocket-WatchOS.
+FOUNDATION_EXPORT const unsigned char SwiftWebSocket_WatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWebSocket_WatchOS/PublicHeader.h>
+
+
